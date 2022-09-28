05cbd0a0d807dbf711ef34390054edaf6b532c6d
sandeep <kotha.sandeep5@gmail.com>
2022-09-28 02:04:19 +0100

    resource group with storage account

---
+++ main/main.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+    features {}
+}
+    resource "azurerm_resource_group" "rg" {
+    name       =  var.rg_name
+    location   =  var.location
+    tags       =  var.tags
+}
+
+
+
+resource "azurerm_storage_account" "st" {
+  name                     = "yakkantisa1419"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "test"
+  }
+}
---
+++ main/variables.tf
@@ -0,0 +1,17 @@
+variable "rg_name" {
+    description = "Resource group name"
+    default     = "saiRG-SA"
+}
+
+variable "location" {
+    description = "location where the Resource will be created"
+    default     = "uksouth"
+}
+
+    variable "tags" {
+    description = "Tags for Resource"
+    type     =  map(string)
+    default = {
+           "enviroment" = "test"
+      }
+}
